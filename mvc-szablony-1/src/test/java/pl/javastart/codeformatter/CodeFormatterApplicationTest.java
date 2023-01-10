8def57e4148acc305aa8ed994dc8aa5d55f21e45
Slawek Ludwiczak <slawekwch@gmail.com>
2023-01-10 09:54:11 +0100

    Spring Boot 3 - Szablony MVC

---
+++ mvc-szablony-1/src/test/java/pl/javastart/codeformatter/CodeFormatterApplicationTest.java
@@ -0,0 +1,12 @@
+package pl.javastart.codeformatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFormatterApplicationTest {
+    @Test
+    void contextLoads() {
+
+    }
+}

c53f6502f05a9f8c5e8e77a20ceaeb9d6160cf6b
Slawek Ludwiczak <slawekwch@gmail.com>
2023-03-09 13:20:53 +0100

    Dane testowe - import.sql zamiast data.sql i testy

--- data-dane-testowe-1/src/test/java/pl/javastart/namestat/TestDataApplicationTest.java
+++ data-dane-testowe-1/src/test/java/pl/javastart/namestat/TestDataApplicationTest.java
@@ -1,13 +1,28 @@
 package pl.javastart.namestat;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @SpringBootTest
 class TestDataApplicationTest {
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        Statement statement = dataSource.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery("select count(*) from name_stats");
+        resultSet.next();
+        int count = resultSet.getInt(1);
+        Assertions.assertEquals(1017, count);
     }
 
 }
